ffde7781396bc459096ae8eb32bf668cd06b5431
YPINPIN <yping611@gmail.com>
2026-03-28 23:38:01 +0800

    add: add background watermark

--- src/pages/frontend/FrontendLayout.tsx
+++ src/pages/frontend/FrontendLayout.tsx
@@ -11,7 +11,15 @@ function FrontendLayout() {
       <ScrollToTop />
 
       <FrontendHeader />
-      <main className="flex-fill bg-light mt-navbar">
+      <main
+        className="flex-fill mt-navbar"
+        style={{
+          backgroundColor: '#f3f4f6',
+          backgroundImage: `url('https://res.cloudinary.com/ddqh0enf7/image/upload/o_4/v1774711367/small-icon-3_kdwhcm.png')`,
+          backgroundRepeat: 'repeat',
+          backgroundSize: '280px 280px',
+        }}
+      >
         <Outlet />
       </main>
       <FrontendFooter />
